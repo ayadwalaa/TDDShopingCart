6aec11f23f7f81b616239790a3787e8a920c55d4
ayadwalaa <ayadwalaa@gmail.com>
2019-02-16 16:16:50 +0200

    My TDD Shopping Cart for SE course - 2019.

---
+++ eclipse-workspace/ShoppingCartXP/src/ShoppingCart.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ShoppingCart {
+	private List<Product> myProducts = new ArrayList <>();
+	private double totalValue;
+
+	public int getNumberOfProducts() {
+		return myProducts.size();
+		}
+	//
+	
+	public double getTotalValue() {
+		// TODO Auto-generated method stub
+		if(myProducts.size()>0) {
+			for(Product product: myProducts) {
+				totalValue = totalValue+ product.getTotalPrice();}
+			}
+		return totalValue;
+	}
+	//
+	
+
+	public void addProduct(Product product) {
+		myProducts.add(product);
+	}
+	
+	}
+
